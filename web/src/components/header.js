5d7703f4dfe2d6a5a418f07a52812e2d85f6bf2c
Are <are.w.sandvik@gmail.com>
2020-04-17 20:29:29 +0200

    remove archive

--- web/src/components/header.js
+++ web/src/components/header.js
@@ -1,9 +1,8 @@
-import {Link} from 'gatsby'
-import React from 'react'
-import Icon from './icon'
-import {cn} from '../lib/helpers'
+import {Link} from 'gatsby';
+import React from 'react';
+import Icon from './icon';
 
-import styles from './header.module.css'
+import styles from './header.module.css';
 
 const Header = ({onHideNav, onShowNav, showNav, siteTitle}) => (
   <div className={styles.root}>
@@ -15,14 +14,6 @@ const Header = ({onHideNav, onShowNav, showNav, siteTitle}) => (
       <button className={styles.toggleNavButton} onClick={showNav ? onHideNav : onShowNav}>
         <Icon symbol='hamburger' />
       </button>
-
-      <nav className={cn(styles.nav, showNav && styles.showNav)}>
-        <ul>
-          <li>
-            <Link to='/archive/'>Archive</Link>
-          </li>
-        </ul>
-      </nav>
     </div>
   </div>
 )
